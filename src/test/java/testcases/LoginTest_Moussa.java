bd62ac73f7bcc1ca816ac01552c13858220e3576
abduben <abduben12@gmail.com>
2023-03-28 21:15:07 -0400

    the login test cases for Moussa

--- src/test/java/testcases/LoginTest_Moussa.java
+++ src/test/java/testcases/LoginTest_Moussa.java
@@ -1,5 +1,14 @@
 package testcases;
 
+import org.testng.annotations.Test;
+
 public class LoginTest_Moussa {
+	
+	@Test
+	
+	public void login() {
+		
+	System.out.println("the login test case for Moussa");
+	}
 
 }
