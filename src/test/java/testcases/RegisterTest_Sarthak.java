cdd474e3329f7529106d591282a42a9d849b9f92
seleniumpandaofficial <seleniumpanda@gmail.com>
2023-03-28 21:22:32 -0400

    Sarthak added one more test case in RegisterTest class

--- src/test/java/testcases/RegisterTest_Sarthak.java
+++ src/test/java/testcases/RegisterTest_Sarthak.java
@@ -4,9 +4,15 @@ import org.testng.annotations.Test;
 
 public class RegisterTest_Sarthak {
 	
-	@Test
+	@Test(priority = 1)
 	public void registerTestCase() {
 		System.out.println("This is register test case");
 	}
+	
+	@Test
+	public void registerLocalTest() {
+		System.out.println("This is local register test case");
+	}
+	
 
 }
