a0f377fb6df52e50aaa68fa1dc28a75f7fabb2e0
seleniumpandaofficial <seleniumpanda@gmail.com>
2023-03-28 21:07:46 -0400

    Register Code scripted and pushed to master by Sarthak

---
+++ src/test/java/testcases/RegisterTest_Sarthak.java
@@ -0,0 +1,12 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class RegisterTest_Sarthak {
+	
+	@Test
+	public void registerTestCase() {
+		System.out.println("This is register test case");
+	}
+
+}
