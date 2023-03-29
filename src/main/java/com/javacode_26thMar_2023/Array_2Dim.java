a34a900f76d01a59a5f2f41333f41d4da8bffe02
seleniumpandaofficial <seleniumpanda@gmail.com>
2023-03-28 20:55:27 -0400

    trying to collaborate in this project for weekday batch

---
+++ src/main/java/com/javacode_26thMar_2023/Array_2Dim.java
@@ -0,0 +1,10 @@
+package com.javacode_26thMar_2023;
+
+public class Array_2Dim {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/main/java/com/javacode_26thMar_2023/Jack_Array.java
@@ -0,0 +1,10 @@
+package com.javacode_26thMar_2023;
+
+public class Jack_Array {
+
+	public static void main(String[] args) {
+		String[] jack = {"hello", "world"};
+
+	}
+
+}
